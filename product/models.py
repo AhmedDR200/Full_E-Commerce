5e5c3f4d23f827c46b602c35dad3183b49a49e9c
AhmedDR200 <alshwwhy212@gmail.com>
2023-11-07 14:01:55 +0200

    Ecommerce Products Model

---
+++ product/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.TextChoices):
+    ACTIVITY = "activity"
+    BUSINESS_AND_FINANCE = "business and finance"
+    EDUCATIONAL_RESOURCES = "educational resources"
+    ENTERTAINMENT = "entertainment"
+    HEALTHCARE = "healthcare"
+    HOME_IMPROVEMENT = "home improvement"
+    INDUSTRY_NEWS = "industry news"
+    LIFESTYLE = "lifestyle"
+    NEWS = "news"
+    POLITICS = "politics"
+    RELIGION = "religion"
+    SCIENCE = "science"
+    SPORTS = "sports"
+    TECHNOLOGY = "technology"
+    TRAVEL = "travel"
+    WORLD = "world"
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=120, default='',blank=False)
+    price = models.DecimalField(decimal_places=2, max_digits=5, default=0)
+    description = models.TextField(max_length=200, blank=False)
+    rate = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    stock = models.IntegerField(default=0)
+    brand = models.CharField(max_length=50, default='', blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=50, choices=Category.choices)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
--- project/settings.py
+++ project/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+
 """
 
 from pathlib import Path
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'product',
 ]
 
 MIDDLEWARE = [
